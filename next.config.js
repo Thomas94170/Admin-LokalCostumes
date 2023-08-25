ef827efa79fb42c0fd5aa552bb0db1311422f5e6
Thomas Thirion <thirionthomas68100@gmail.com>
2023-08-25 21:15:04 +0200

    resolution recuperation image du back end

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["localhost"],
+  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'http',
+  //       hostname: 'localhost',
+  //       port: '5400',
+  //       pathname: '/account123/**',
+  //     },
+  //   ],
+  // },
+};
+
+//lors du deploy hostname sera le serveur
+// https://github.com/vercel/next.js/discussions/18311#discussioncomment-2934096
